122762574db7068a4ed7302fbd2f7f0b81a2f075
Sahil <sahilrox528@gmail.com>
2025-02-20 15:41:03 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,7 @@
+# Creating Tuples and Performing operations on tuples
+
+my_tuple = (1, 'Sahil', 2.13, True)
+print("Tuple: ", my_tuple)
+print("Tuple: ",my_tuple[2])
+subtuple = my_tuple[1:3]
+print("Subtuple: ", subtuple)
